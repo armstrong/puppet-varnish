4bd698a1fa2d5c04fa7fcc595dc8ff5183cab6d9
Niran Babalola <niran@niran.org>
2011-08-29 21:27:31 -0500

    Make room for a manifests to be provided alongside modules

---
+++ manifests/init.pp
@@ -0,0 +1,49 @@
+class varnish {
+    package { curl:
+        ensure => installed,
+        before => Exec["add_varnish_key"],
+            }
+    exec { "add_varnish_key":
+        command => "curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -",
+        before  => Exec['apt_update'],
+        require => Package['curl'],
+        path    => '/usr/bin',
+    }
+    file { 'apt/sources.list':
+        path    => '/etc/apt/sources.list.d/varnish.list',
+        ensure  => file,
+        before  => Exec['apt_update'],
+        source => "puppet:///modules/varnish/sources.list",
+    }
+    exec { "apt_update":
+        command => "sudo apt-get update",
+        before  => Package['varnish'],
+        path    => '/usr/bin',
+    }
+    package { varnish:
+        ensure  => latest,
+        require => Exec["apt_update"],
+    }
+
+    file { 'texastribune.org.vcl':
+        path    => '/etc/varnish/texastribune.org.vcl',
+        ensure  => file,
+        require => Package['varnish'],
+        before  => Service['varnish'],
+        content => template("varnish/texastribune.org.vcl"),
+    }
+    file { 'defaults':
+        path    => '/etc/default/varnish',
+        ensure  => file,
+        require => Package['varnish'],
+        before  => Service['varnish'],
+        content => template("varnish/defaults"),
+    }
+    service { varnish:
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+    }
+}
+
+include varnish
